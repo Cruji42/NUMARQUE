9f4f95730d76ce579bbc058a74ddd1dfe5abb808
NupeDev <cruji42@gmail.com>
2026-03-09 22:00:57 -0600

    ci: add GitHub Pages deploy workflow

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://137.184.132.38:8008/api/v1',
+  // apiUrl:'http://localhost:8000/api/v1',
+  GOOGLE_CLIENT_ID: "1096513180722-d9ecv4gllkmiscrcgsnkf2uf1da3hnj3.apps.googleusercontent.com"
 };
