358166562fd08e6235200a8df0aefd062fe1cef7
NupeDev <cruji42@gmail.com>
2026-03-09 23:08:36 -0600

    change name to numarque

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('enlink app is running!');
+    expect(await page.getTitleText()).toEqual('numarque app is running!');
   });
 
   afterEach(async () => {
